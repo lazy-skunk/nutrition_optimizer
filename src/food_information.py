58d59fdcdf5173bff2ee361be82d6b42c66c3bd5
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-12-22 13:14:29 +0000

    Refactor: Overall validation updates.

--- src/food_information.py
+++ src/food_information.py
@@ -19,35 +19,19 @@ class FoodInformation:
     NUTRIENT_COMPONENTS = ["energy", "protein", "fat", "carbohydrates"]
 
     def __post_init__(self) -> None:
-        self._validate_non_none_values()
+        self._validate_name_is_not_blank()
         self._validate_nutrient_values_are_non_negative()
         self._validate_grams_per_unit_is_greater_than_zero()
         self._validate_intake_values_are_non_negative()
         self._validate_minimum_intake_is_less_than_maximum_intake()
 
-    def _validate_non_none_values(self) -> None:
-        if any(
-            value is None
-            for value in [
-                self.name,
-                self.energy,
-                self.protein,
-                self.fat,
-                self.carbohydrates,
-                self.grams_per_unit,
-                self.minimum_intake,
-                self.maximum_intake,
-            ]
-        ):
-            raise ValueError(
-                "All attributes (food name, energy, protein, fat,"
-                " carbohydrates, grams per unit, minimum intake,"
-                " and maximum intake) must be provided. None value found."
-            )
+    def _validate_name_is_not_blank(self) -> None:
+        if not self.name.strip():
+            raise ValueError("Food name must be provided.")
 
     def _validate_nutrient_values_are_non_negative(self) -> None:
         if any(
-            value < 0
+            value is None or value < 0
             for value in [
                 self.energy,
                 self.protein,
@@ -61,7 +45,7 @@ class FoodInformation:
             )
 
     def _validate_grams_per_unit_is_greater_than_zero(self) -> None:
-        if self.grams_per_unit <= 0:
+        if self.grams_per_unit is None or self.grams_per_unit <= 0:
             raise ValueError(
                 f"Invalid grams per unit for {self.name}."
                 " It must be greater than zero."
@@ -69,7 +53,8 @@ class FoodInformation:
 
     def _validate_intake_values_are_non_negative(self) -> None:
         if any(
-            value < 0 for value in [self.minimum_intake, self.maximum_intake]
+            value is None or value < 0
+            for value in [self.minimum_intake, self.maximum_intake]
         ):
             raise ValueError(
                 f"Invalid intake values for {self.name}."
