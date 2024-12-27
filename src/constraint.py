1d09fa6652e0be616b1e3c7a9bc0e8129ab5c57e
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-12-27 09:48:57 +0000

    Update: Implement logging.

--- src/constraint.py
+++ src/constraint.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 
 from src.food_information import FoodInformation
+from src.singleton_logger import SingletonLogger
+
+_logger = SingletonLogger.get_logger()
 
 
 @dataclass(frozen=True)
@@ -14,6 +17,11 @@ class Constraint:
     UNITS = ["amount", "energy", "ratio"]
 
     def __post_init__(self) -> None:
+        _logger.debug(
+            f"Initializing Constraint with min_max={self.min_max},"
+            f" nutritional_component={self.nutritional_component},"
+            f" unit={self.unit}, value={self.value}"
+        )
         self._validate_min_max()
         self._validate_nutritional_component()
         self._validate_unit()
@@ -21,30 +29,47 @@ class Constraint:
 
     def _validate_min_max(self) -> None:
         if self.min_max not in self.MIN_MAX:
-            raise ValueError(
+            error_message = (
                 f"Invalid Min/Max value: {self.min_max}."
                 f" Valid values are {Constraint.MIN_MAX}."
             )
+            _logger.error(error_message)
+            raise ValueError(error_message)
+        _logger.info(f"Min/Max value '{self.min_max}' is valid.")
 
     def _validate_nutritional_component(self) -> None:
         if (
             self.nutritional_component
             not in FoodInformation.NUTRIENT_COMPONENTS
         ):
-            raise ValueError(
+            error_message = (
                 f"Invalid nutritional component: {self.nutritional_component}."
                 f" Valid components are {FoodInformation.NUTRIENT_COMPONENTS}."
             )
+            _logger.error(error_message)
+            raise ValueError(error_message)
+
+        _logger.info(
+            f"Nutritional component '{self.nutritional_component}' is valid."
+        )
 
     def _validate_unit(self) -> None:
         if self.unit not in self.UNITS:
-            raise ValueError(
+            error_message = (
                 f"Invalid unit: {self.unit}."
                 f" Valid units are {Constraint.UNITS}."
             )
+            _logger.error(error_message)
+            raise ValueError(error_message)
+
+        _logger.info(f"Unit '{self.unit}' is valid.")
 
     def _validate_value_is_non_negative(self) -> None:
         if self.value is None or self.value < 0:
-            raise ValueError(
+            error_message = (
                 f"Constraint value must be non-negative. Got {self.value}."
             )
+            _logger.error(error_message)
+            raise ValueError(error_message)
+
+        _logger.info(f"Value '{self.value}' is valid and non-negative.")
