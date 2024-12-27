1da6a7c1abd39a05ba14d5dfad12f2c09d2ee090
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-12-27 13:53:50 +0000

    Refactor: Improved initialize for clarity.

--- src/singleton_logger.py
+++ src/singleton_logger.py
@@ -1,59 +1,71 @@
 import logging
+import os
+from logging import Formatter, Logger, StreamHandler
 from logging.handlers import RotatingFileHandler
-from typing import Any, Optional
 
 _DEFAULT_LOG_LEVEL = logging.DEBUG
 _DEFAULT_LOG_PATH = "log/app.log"
-_DEFAULT_LOG_SIZE = 1024 * 1024 * 5
-_DEFAULT_LOG_BACKUP = 5
+_DEFAULT_LOG_SIZE = 1024 * 1024 * 10
+_DEFAULT_LOG_BACKUP = 3
 
 
 class SingletonLogger:
-    _instance: Optional["SingletonLogger"] = None
-    _logger: Optional[logging.Logger] = None
-
-    def __new__(cls, *args: Any, **kwargs: Any) -> "SingletonLogger":
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._initialize_logger(*args, **kwargs)
-        return cls._instance
-
-    def _initialize_logger(
-        self,
-        log_level: int = _DEFAULT_LOG_LEVEL,
-        log_path: str = _DEFAULT_LOG_PATH,
-        log_size: int = _DEFAULT_LOG_SIZE,
-        log_backup: int = _DEFAULT_LOG_BACKUP,
-    ) -> None:
-        if SingletonLogger._logger is None:
-            SingletonLogger._logger = logging.getLogger("nutrition_optimizer")
-            SingletonLogger._logger.setLevel(log_level)
-
-            console_handler = logging.StreamHandler()
-            console_handler.setLevel(log_level)
-
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S",
-            )
-            console_handler.setFormatter(formatter)
-            SingletonLogger._logger.addHandler(console_handler)
-
-            file_handler = RotatingFileHandler(
-                log_path, maxBytes=log_size, backupCount=log_backup
-            )
-            file_handler.setLevel(logging.DEBUG)
-            file_handler.setFormatter(formatter)
-            SingletonLogger._logger.addHandler(file_handler)
+    _logger: Logger | None = None
 
     @classmethod
-    def get_logger(cls) -> logging.Logger:
-        if cls._instance is None:
-            cls._instance = cls()
+    def _get_log_level(cls) -> str | int:
+        log_level = os.getenv("LOG_LEVEL", _DEFAULT_LOG_LEVEL)
+        return log_level
 
+    @classmethod
+    def _add_stream_handler(
+        cls, log_level: str | int, formatter: Formatter
+    ) -> None:
         if cls._logger is None:
-            raise RuntimeError(
-                "Unexpected error occurred. Logger is not initialized."
+            raise RuntimeError("Logger has not been initialized.")
+
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(log_level)
+        stream_handler.setFormatter(formatter)
+
+        cls._logger.addHandler(stream_handler)
+
+    @classmethod
+    def _add_rotating_file_handler(
+        cls, log_level: str | int, formatter: Formatter
+    ) -> None:
+        if cls._logger is None:
+            raise RuntimeError("Logger has not been initialized.")
+
+        log_path = os.getenv("LOG_PATH", _DEFAULT_LOG_PATH)
+        log_size = int(os.getenv("LOG_SIZE", _DEFAULT_LOG_SIZE))
+        log_backup = int(os.getenv("LOG_BACKUP", _DEFAULT_LOG_BACKUP))
+
+        rotating_file_handler = RotatingFileHandler(
+            log_path, maxBytes=log_size, backupCount=log_backup
+        )
+        rotating_file_handler.setLevel(log_level)
+        rotating_file_handler.setFormatter(formatter)
+
+        cls._logger.addHandler(rotating_file_handler)
+
+    @classmethod
+    def initialize(cls) -> None:
+        if cls._logger is None:
+            cls._logger = logging.getLogger("nutrition_optimizer")
+
+            log_level = cls._get_log_level()
+            cls._logger.setLevel(log_level)
+
+            formatter = Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
             )
+            cls._add_stream_handler(log_level, formatter)
+            cls._add_rotating_file_handler(log_level, formatter)
+
+    @classmethod
+    def get_logger(cls) -> Logger:
+        if cls._logger is None:
+            raise RuntimeError("Logger has not been initialized.")
 
         return cls._logger
